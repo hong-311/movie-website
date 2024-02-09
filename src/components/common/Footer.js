0bef6accb0da6a489872434ef8de9c9fd5618791
hong-311 <136770134+hong-311@users.noreply.github.com>
2024-02-10 00:46:10 +0900

    반응형 구현
    반응형 구현하였습니다

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -8,10 +8,6 @@ import GROUP from '../../apis/group.json';
 const FooterBlock = styled.footer`
     width: 100%; padding: 0 60px; 
 
-    @media screen and (max-width: 768px) {
-        padding: 0 30px;
-    }
-
     .f_top{
         width: 100%; height: 60px; line-height: 60px;
         border-bottom: 1px solid #222;
@@ -19,43 +15,40 @@ const FooterBlock = styled.footer`
     }
     .notice{
         display: flex; 
+        white-space: nowrap;
+        overflow: visible; 
         h3{
             margin-right: 20px;
-            font-size: 15px; font-weight: normal; color: rgb(110, 110, 110);
+            font-size: 18px; font-weight: normal; color: rgb(110, 110, 110);
         }
-        a{ font-size: 15px; font-weight: bold; color: rgb(163, 163, 163); }
+        a{ font-size: 18px; font-weight: bold; color: rgb(163, 163, 163); }
     }
     .family{
         ul{
             display: flex;
-            
             li{
-                margin-left: 30px;
-                font-size: 15px; font-weight: normal; color: rgb(163, 163, 163);
-        
+                margin-left: 20px;
+            
                 select {
+                    font-size: 18px; font-weight: bold; color: rgb(163, 163, 163);
                     background-color: black;
-                    border-radius: 5px;
+                    border-style: none;
                     appearance: none;
-    
+                    white-space: nowrap;
                 }
                 option{
-                    font-size: 13px; font-weight: bold; color: rgb(163, 163, 163);
-                    text-align: left;
-                    width: 7.3rem;
-                    padding: 8px 15px;
+                    font-size: 18px; font-weight: bold; color: rgb(163, 163, 163);
                     background-color: rgb(32, 32, 32);
-                    height: 40px;
+                    height: 50px;
                         &:hover {
-                            background-color: rgb(43, 43, 43);
+                            background-color: rgb(50, 50, 50);
                         }
+                    
                 }
-                ::-webkit-scrollbar {
-                    width: 7px; /* 수직 스크롤바의 너비 */
-
-                }
-                ::-webkit-scrollbar-thumb {
-                    background: rgb(76, 75, 77) /* 스크롤바 핸들 배경색 */
+                a{
+                    white-space: nowrap;
+                    overflow: visible; 
+                    font-size: 16px; font-weight: bold; color: rgb(163, 163, 163);
                 }
             }
         }
@@ -63,16 +56,18 @@ const FooterBlock = styled.footer`
     .f_bottom{
         padding-bottom: 130px;
         .f_nav{
+            white-space: nowrap;
+            overflow: visible; 
             display: flex; height: 60px; line-height: 60px;
             li{
-                margin-right: 20px;
-                font-size: 15px; color: rgb(163, 163, 163);
+                margin-right: 30px;
+                font-size: 18px; color: rgb(163, 163, 163);
                 &:nth-child(3){ font-weight: bold; }
             }
         }
         .footer_text{
             span{
-                font-size: 13px; font-weight: normal; 
+                font-size: 16px; font-weight: normal; 
                 color: rgb(110, 110, 110); line-height: 24px;
                 margin-right: 20px;
                 &::after{
@@ -83,7 +78,7 @@ const FooterBlock = styled.footer`
             }
         }
         .copy{
-            font-size: 13px; font-weight: normal; 
+            font-size: 16px; font-weight: normal; 
             color: rgb(110, 110, 110); line-height: 24px;
         }
         .sns{
@@ -91,17 +86,54 @@ const FooterBlock = styled.footer`
             li{ margin-right: 10px; } 
         }
     }
+
+    @media screen and (max-width: 768px) {
+        padding: 0 10px;
     
+        .f_top h3,
+        .f_top a,
+        .family ul li a,
+        .f_bottom .f_nav li,
+        .f_bottom .footer_text span,
+        .f_bottom .copy {
+            font-size: 14px;
+            margin-bottom: 10px;
+        }
+
+        .f_bottom .sns li img {
+            width: 30px; 
+            height: auto;
+            margin-bottom: 10px; 
+        }
+    }
+    @media screen and (max-width: 512px) {
+        padding: 0 10px;
+    
+        .f_top h3,
+        .f_top a,
+        .family ul li a,
+        .f_bottom .f_nav li,
+        .f_bottom .footer_text span,
+        .f_bottom .copy {
+            font-size: 12px;
+        }
+
+        .f_bottom .sns li img {
+            width: 25px; 
+            
+    }
+
 `;
+  
 
 //footer파트 컴포넌트
 function Footer({brand}) {
     //텍스트 배열데이터
     const footerMenus = ['고객센터','이용약관','개인정보처리방침','청소년 보호정책','법적고지','이벤트','인재채용'];
     const footerTexts = [
-        ['대표이사 : 최주희','사업자정보확인','사업자등록번호 : 188-88-01893','통신판매신고번호 : 2020-서울마포-3641호'],
+        ['대표이사 : YANG JIEUL','사업자정보확인','사업자등록번호 : 188-88-01893','통신판매신고번호 : 2020-서울마포-3641호'],
         ['사업장 : 서울특별시 마포구 상암산로 34, DMC디지털큐브 15층(상암동)','호스팅사업자 : 씨제이올리브네트웍스(주)'],
-        ['고객센터 (평일 09시~17시/점심시간 13시~14시)','1:1 게시판 문의','대표메일 : tving@cj.net','전화번호(ARS) 1670-1525 (챗봇/채팅 상담 연결)'],
+        ['고객문의 바로가기','대표메일 : tving@cj.net','고객센터 : 1670-1525 (평일/주말 09시~18시, 공휴일 휴무)'],
         ['ENM 시청자 상담실 (편성 문의 및 시청자 의견) : 080-080-0780','Mnet 고객센터(방송편성문의) : 1855-1631']
     ];
 
@@ -128,17 +160,17 @@ function Footer({brand}) {
           }
         };
   
-    // 외부 영역 클릭 시 셀렉트 박스 닫기
-    useEffect(() => {
-        function handleClickOutside(event) {
-        if (
-            (brandSelectRef.current && !brandSelectRef.current.contains(event.target) && brandOpen) ||
-            (groupSelectRef.current && !groupSelectRef.current.contains(event.target) && groupOpen)
-        ) {
-            setBrandOpen(false);
-            setGroupOpen(false);
-        }
-        }
+      // 외부 영역 클릭 시 셀렉트 박스 닫기
+     useEffect(() => {
+     function handleClickOutside(event) {
+      if (
+        (brandSelectRef.current && !brandSelectRef.current.contains(event.target) && brandOpen) ||
+        (groupSelectRef.current && !groupSelectRef.current.contains(event.target) && groupOpen)
+      ) {
+        setBrandOpen(false);
+        setGroupOpen(false);
+      }
+    }
 
     // 이벤트 추가
     document.addEventListener("mousedown", handleClickOutside);
@@ -148,26 +180,25 @@ function Footer({brand}) {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [brandOpen, groupOpen]);
-
-  
   
     return (
         <FooterBlock>
             <div className="f_top">
                 <div className="notice">
                     <h3>공지사항</h3>
-                    <a href="#!">[안내] 개인정보처리방침 개정 안내 (2023년 10월 3일)</a>
+                    <a href="#!">[안내] 개인정보처리방침 개정 안내 (2023년 3월 3일)</a>
                 </div>
                 <div className="family">
                     <ul>
-                        <li onClick={() => setBrandOpen(!brandOpen)}> 브랜드 바로가기 +
+                        <li>
+                            <a href="#!" onClick={() => setBrandOpen(!brandOpen)}> 브랜드 바로가기 +
                             <div
                                 ref={brandSelectRef} 
                                 className="select-container"
                             >
                                 <select
-                                    onChange={(e) => handleSelect(e, "brand")} value={selected}
-                                    size={8}
+                                     onChange={(e) => handleSelect(e, "brand")} value={selected}
+                                    size={7}
                                     style={{ display: brandOpen ? 'block' : 'none'}}
                                 >
                                     {BRAND.brand.map((brand) => (
@@ -177,15 +208,17 @@ function Footer({brand}) {
                                     ))}
                                 </select>
                             </div>
-                         </li>
-                        <li onClick={() => setGroupOpen(!groupOpen)}>그룹 계열사 바로가기 +
+                        </a>
+                      </li>
+                        <li>
+                            <a href="#!" onClick={() => setGroupOpen(!groupOpen)}>그룹 계열사 바로가기 +
                             <div
                                 ref={groupSelectRef}
                                 className="select-container"
                             >
                                 <select
                                      onChange={(e) => handleSelect(e, "group")} value={selected}
-                                    size={8}
+                                    size={7}
                                     style={{ display: groupOpen ? 'block' : 'none' }}
                                 >
                                     {GROUP.group.map((group) => (
@@ -195,6 +228,7 @@ function Footer({brand}) {
                                     ))}
                                 </select>
                             </div>
+                        </a>
                         </li>
                     </ul>
                 </div>
